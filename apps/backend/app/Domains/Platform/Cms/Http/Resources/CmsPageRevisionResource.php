5f1e6f27e9ccb0c37d92abb21ebf073941f855e8
Md Zahedul Hasan <afzatube@gmail.com>
2026-09-07 01:47:46 +0600

    fix(cms): type revision API resource

--- apps/backend/app/Domains/Platform/Cms/Http/Resources/CmsPageRevisionResource.php
+++ apps/backend/app/Domains/Platform/Cms/Http/Resources/CmsPageRevisionResource.php
@@ -4,11 +4,14 @@ declare(strict_types=1);
 
 namespace App\Domains\Platform\Cms\Http\Resources;
 
+use App\Domains\Platform\Cms\Models\CmsPageRevision;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/** @mixin CmsPageRevision */
 final class CmsPageRevisionResource extends JsonResource
 {
+    /** @return array<string, mixed> */
     public function toArray(Request $request): array
     {
         return [
